d99e67aaf74684f7316324dbd31ae4d15a597015
Zheng Qichun <zqc111123@gmail.com>
2021-05-04 20:08:34 +0800

    2nd commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const bodyParser = require("body-parser");
+const http = require('http');
+const path = require("path");
+const webSocket = require('ws');
+const express = require('express');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const server = http.createServer(app);//create a server
+
+const s = new webSocket.Server({server});
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+s.on('connection',function (ws,req){
+    ws.on('message',function (message){
+        console.log("Received:"+message);
+        s.clients.forEach(function (client){
+            if(client!==ws && client.readyState){
+                client.send(message);
+            }
+        });
+    });
+    ws.on('close',function (){
+        console.log("lost one client");
+    });
+    console.log("new client connected");
+});
+server.listen(3000);
